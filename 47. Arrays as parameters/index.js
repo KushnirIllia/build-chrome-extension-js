550619119296d31c8a65c91af6ddc6eab957bb51
KushnirIllia <illiakushnira@gmail.com>
2022-12-17 22:17:55 +0200

    Arguments vs Parameters. Arrays as parameters

---
+++ 47. Arrays as parameters/index.js
@@ -0,0 +1,9 @@
+// Create a function, getFirst(arr), that returns the first item in the array
+
+const getFirst = (arr) => arr[0];
+
+const array = ["Illia", "Kushnir"];
+
+// Call it with an array as an argument to verify that it works
+
+console.log(getFirst(array));
